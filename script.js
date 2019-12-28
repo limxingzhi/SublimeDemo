caa6e2a0251793053560b1423560af2e7e24c9f7
Lim Xing Zhi <coldsandstorm97@gmail.com>
2019-12-28 20:05:27 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Sends a browser alert
+ * @param  {[string]} inputString The String to Input
+ */
+function demoAlert (inputString) {
+	window.alert(inputString);
+};
+
+demoAlert("hello world");
